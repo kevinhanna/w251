c8e22b56b4ff3a3e961cf2824bef58128a097328
Kevin Hanna <kevinhanna@gmail.com>
2020-01-19 01:17:19 -0500

    Working

--- 3hw/containers/local_mqtt_forwarder/python/local_broker.py
+++ 3hw/containers/local_mqtt_forwarder/python/local_broker.py
@@ -4,24 +4,23 @@ import time
 remote_client = mqtt.Client()
 # This is the Subscriber
 
+remote_client.connect("169.53.167.199",1883,60)
 def post_face_remote(face):
-    remote_client.connect("169.53.167.199",1883,20)
+    #remote_client.connect("169.53.167.199",1883,60)
+    #remote_client.connect("169.53.167.193",1883,60)
     try:
         remote_client.publish("topic/test", payload=face, qos=0, retain=False);
     except:
         print("Unexpected error:", sys.exc_info()[0])
-    remote_client.disconnect();
+    #remote_client.disconnect();
 
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code "+str(rc))
-  client.subscribe("topic/test")
+  client.subscribe("topic/test",qos=1)
 
 def on_message(client, userdata, msg):
-#    if (len(msg.payload) < 50000):
     print("Forwarding face " + str(len(msg.payload)))
     post_face_remote(msg.payload)
-#    else:
-#        print("NOT forwarding face " + str(len(msg.payload)))
 
 client = mqtt.Client(transport="tcp")
 
