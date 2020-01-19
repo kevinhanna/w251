3eda9d421c32308ee5e7d9092d087829bc73ea79
Kevin Hanna <kevinhanna@gmail.com>
2020-01-19 18:21:42 -0500

    new bucket, new topic

--- 3hw/containers/face_detector/python/test.py
+++
@@ -1,8 +0,0 @@
-import paho.mqtt.client as mqtt
-
-# This is the Publisher
-
-client = mqtt.Client()
-client.connect("local_mqtt_broker",1883,60)
-client.publish("topic/test", "Hello world!");
-client.disconnect();
